be7966cc58b9e612c885f7aa28c9b71f72b8dc34
Dave The Web Dev <dantczak211@law.edu.pl>
2021-04-02 09:27:48 +0200

    with Reduce

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 import { inputData } from './task/utilis';
 import { findPharseInArray } from './task/index';
 
-findPharseInArray(inputData, 'dup');
+findPharseInArray(inputData, 'dupddddd');
--- task/index.js
+++ task/index.js
@@ -1,10 +1,24 @@
+// export const findPharseInArray = (array, pharse) => {
+//   const results = []
+//   array.filter((word, id) => {
+//     const wordToPush = word.toLowerCase().includes(pharse.toLowerCase())
+//     if (wordToPush) {
+//         results.push([id,  word])
+//       }
+//     })
+//   return results
+// };
+
 export const findPharseInArray = (array, pharse) => {
-  const results = []
-  array.filter((word, id) => {
-    const wordToPush = word.toLowerCase().includes(pharse)
-    if (wordToPush) {
-        results.push([id,  word])
-      }
-    })
+  let results = array.reduce((accumulator, currentValue, currentIndex) => {
+    if (currentValue.toLowerCase().includes(pharse.toLowerCase())) {
+      accumulator.push({place: currentIndex, word: currentValue})
+    }
+    return accumulator
+  }, [])
+  if (results.length === 0) {
+    results.push({message: "No word includes given pharse"})
+  }
+  console.log(results)
   return results
 };
