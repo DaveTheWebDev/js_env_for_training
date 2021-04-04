c931a2e22ac8e51a91a15f7f46c75ca2034bca39
Dave The Web Dev <dantczak211@law.edu.pl>
2021-04-04 14:37:13 +0200

    getMyAge

--- app.js
+++ app.js
@@ -1,57 +1,12 @@
-import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
-
-const generateNameAndSurname = () => {
-  return axios
-    .get('https://randomuser.me/api/')
-    .then((res) => res.data.results[0].name)
-    .catch((err) => console.log(err));
-};
-const generateId = () => {
-  return uuidv4();
-};
-const generateAge = (max = 85, min = 18) => {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-const generatePhoneNumber = (numberLength = 9) => {
-  let phoneNumber = '';
-  for (let index = 0; index < numberLength; index++) {
-    const number = Math.floor(Math.random() * 10);
-    phoneNumber += number;
+const getMyAge = (date) => {
+  const currentYear = new Date().getFullYear();
+  if (typeof date === 'string') {
+    return currentYear - Number(date);
+  } else if (typeof date === 'number') {
+    return currentYear - date;
+  } else if (date instanceof Date) {
+    return currentYear - date;
   }
-  return phoneNumber;
-};
-const generateCountry = () => {
-  return axios
-    .get('https://restcountries.eu/rest/v2/all?fields=name')
-    .then((res) => {
-      const countries = res.data;
-      const randomId = Math.floor(Math.random() * countries.length);
-      return countries[randomId].name;
-    })
-    .catch((err) => console.log(err));
 };
 
-const generateHuman = async () => {
-  const names = await generateNameAndSurname();
-  const name = names.first;
-  const surname = names.last;
-  const email = `${name}${surname}@test.pl`;
-  const age = generateAge();
-  const phone = generatePhoneNumber();
-  const country = await generateCountry();
-  const id = generateId();
-  const human = {
-    name,
-    surname,
-    email,
-    age,
-    phone,
-    country,
-    id,
-  };
-  console.log(human);
-  return human;
-};
-
-generateHuman();
+getMyAge(new Date(2009));
