cfe48ca98d5f2425fb025463240f1ce420c2ccf0
Dave The Web Dev <dantczak211@law.edu.pl>
2021-05-12 16:12:14 +0200

    AdressBook

--- app.js
+++ app.js
@@ -1,46 +1,130 @@
-const mapCb = (x) => x * 2;
-const arr = [2, 3, 4, 5];
-const mapFn = (array, cb) => {
-  return array.reduce((acc, val) => {
-    acc.push(cb(val));
-    return acc;
-  }, []);
-};
+import { v4 as uuidv4 } from 'uuid';
 
-// console.log(mapFn(arr, mapCb));
+class Contact {
+  constructor(name, surname, email) {
+    this.uuid = uuidv4();
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.creationDate = new Date();
+    this.modificationDate = new Date();
+  }
+  updateModificationDate() {
+    this.modificationDate = new Date();
+  }
+  updateName(name) {
+    this.updateModificationDate();
+    this.name = name;
+  }
+  updateSurname(surname) {
+    this.updateModificationDate();
+    this.surname = surname;
+  }
+}
 
-const filterCb = (x) => x > 3;
-const filterFn = (array, cb) => {
-  return array.reduce((acc, val) => {
-    if (cb(val)) {
-      acc.push(val);
+class Group {
+  constructor(name, listArray) {
+    this.uuid = uuidv4();
+    this.name = name;
+    this.list = listArray;
+  }
+  updateListName(listName) {
+    this.name = listName;
+  }
+  addContact(contact) {
+    this.list.push(contact);
+  }
+  removeContact(contact) {
+    const list = [...this.list];
+    const contactId = list.indexOf(contact);
+    list.splice(contactId, 1);
+    this.list = list;
+  }
+  isInGroup(contact) {
+    return !!this.list.find((contactEl) => contactEl.uuid === contact.uuid);
+  }
+}
+
+class AddressBook {
+  constructor(contactsList, groupsList) {
+    this.contactsList = contactsList;
+    this.groupsList = groupsList;
+  }
+
+  addContact(contact) {
+    this.groupsList.push(contact);
+  }
+  removeContact(contact) {
+    const list = [...this.groupsList];
+    const contactIndex = list.findIndex(
+      (contactEl) => contactEl.uuid === contact.uuid
+    );
+    if (contactIndex !== -1) {
+      list.splice(contactIndex, 1);
+      this.groupsList = list;
     }
-    return acc;
-  }, []);
-};
-
-// console.log(filterFn(arr, filterCb));
-
-const everyCb = (x) => x > 1;
-const everyFn = (array, cb, id) => {
-  return array.reduce((acc, val) => {
-    if (!cb(val)) {
-      acc = false;
+  }
+  modifyContact(contact, field, value) {
+    const list = [...this.groupsList];
+    const contactIndex = list.findIndex(
+      (contactEl) => contactEl.uuid === contact.uuid
+    );
+    if (contactIndex === -1) {
+      console.log('nie ma takiego kontakut');
+    } else {
+      if (field in contact) {
+        contact[field] = value;
+      } else {
+        console.log('nie ma tekiego pola');
+      }
     }
-    return acc;
-  }, true);
-};
-
-// console.log(everyFn(arr, everyCb));
-
-const someCb = (x) => x > 10;
-const someFn = (array, cb) => {
-  return array.reduce((acc, val) => {
-    if (cb(val)) {
-      acc = true;
+  }
+  addGroup(group) {
+    this.groupsList.push(group);
+  }
+  removeGroup(group) {
+    const list = [...this.groupsList];
+    const groupIndex = list.findIndex((groupEl) => groupEl.uuid === group.uuid);
+    if (groupIndex !== -1) {
+      list.splice(groupIndex, 1);
+      this.groupsList = list;
     }
-    return acc;
-  }, false);
-};
+  }
+  modifyGroup(group, field, value) {
+    const list = [...this.groupsList];
+    const groupIndex = list.findIndex((groupEl) => groupEl.uuid === group.uuid);
+    if (groupIndex === -1) {
+      console.log('nie ma takiego kontakut');
+    } else {
+      if (field in group) {
+        group[field] = value;
+      } else {
+        console.log('nie ma tekiego pola');
+      }
+    }
+  }
+  findContactByPharse(pharse) {
+    const regExp = new RegExp(pharse, 'gi');
+    const result = this.contactsList.filter((contact) => {
+      const objValues = Object.values(contact);
+      const cheked = objValues.filter((objValue) => {
+        return regExp.test(objValue);
+      });
+      return cheked.length;
+    });
+    console.log(result);
+    return result;
+  }
+}
 
-console.log(someFn(arr, someCb));
+const dupa = new Contact('daw1', 'ant', 'ddd');
+const dupa1 = new Contact('daw2', 'ant', 'ddd');
+const dupa2 = new Contact('daw3', 'ant', 'ddd');
+
+const grupa = new Group('ziomki', [dupa, dupa1, dupa2]);
+
+const listt = new AddressBook([dupa, dupa1, dupa2], [grupa]);
+
+listt.findContactByPharse('ant');
+
+// console.log(listt);
